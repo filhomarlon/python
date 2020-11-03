cee6736967961e6085c0b6a149247e5b8732e24b
MARLON XAVIER <marlon.filho@pmm.am.gov.br>
2020-11-03 12:47:44 -0400

    Add files via upload

---
+++ operacoesMatematicas.py
@@ -0,0 +1,15 @@
+""" + => soma
+    - => subtração
+    * => Multiplicação
+    / => Divisão
+    //=> divisão de resultado inteiro
+    % => modulo ou Resto da divisão
+    ** => potenciação
+"""
+
+x = 12
+y = 10
+soma = x + y
+print("soma eh: ", soma)
+print("soma eh: ", x+y)
+
